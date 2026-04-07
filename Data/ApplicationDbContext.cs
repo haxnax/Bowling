d5810e550786c620af9c1a6e9963ad5372ee527b
haxnax <myyar2012@gmail.com>
2026-04-07 12:36:52 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bowling.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+        public DbSet<deleteme> deleteme { get; set; }
+
+
+        public ApplicationDbContext()
+        {
+            // en tom konstruktor behövs för att skapa migrations
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.;Database=DBBowling;Trusted_Connection=True;TrustServerCertificate=true;");
+            }
+        }
+    }
+
+}
---
+++ Data/Bookings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bowling.Data
+{
+    public class Bookings
+    {
+        public int Id { get; set; }
+        [AllowedValues()]
+        public DateTime BookingDate { get; set; } 
+
+    }
+}
